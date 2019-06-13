480f9d60340c58681288e6e20b26be6b4bc3c198
marco <marco.magnatti@gmail.com>
2019-06-13 22:33:58 +0200

    jwt user

--- src/main/java/com/harlansoft/todo/app/security/jwt/JwtInMemoryUserDetailsService.java
+++ src/main/java/com/harlansoft/todo/app/security/jwt/JwtInMemoryUserDetailsService.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 //@Service
 public class JwtInMemoryUserDetailsService implements UserDetailsService {
